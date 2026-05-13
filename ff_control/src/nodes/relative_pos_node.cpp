d4aee6e38ef1a543e7b20fc44cc383d07453bfd9
Xiangmei Chen <xchen47@stanford.edu>
2026-05-12 23:17:16 -0700

    make it to print relative pos first

---
+++ ff_control/src/nodes/relative_pos_node.cpp
@@ -0,0 +1,79 @@
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <cmath>
+
+class RelativePositionPrinter : public rclcpp::Node
+{
+public:
+    RelativePositionPrinter() : Node("relative_pos_node")
+    {
+        robot1_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+            "/vrpn_mocap/robot/pose", 10,
+            std::bind(&RelativePositionPrinter::robot1_callback, this, std::placeholders::_1));
+
+        robot2_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+            "/vrpn_mocap/robot2/pose", 10,
+            std::bind(&RelativePositionPrinter::robot2_callback, this, std::placeholders::_1));
+        
+        RCLCPP_INFO(this->get_logger(), "Relative Position Printer Node Started");
+    }
+
+private:
+    void robot1_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+    {
+        robot1_pos_ = msg->pose.position;
+        robot1_received_ = true;
+        print_relative_position();
+    }
+    
+    void robot2_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+    {
+        robot2_pos_ = msg->pose.position;
+        robot2_received_ = true;
+        print_relative_position();
+    }
+    
+    void print_relative_position()
+    {
+        if (robot1_received_ && robot2_received_)
+        {
+            double dx = robot2_pos_.x - robot1_pos_.x;
+            double dy = robot2_pos_.y - robot1_pos_.y;
+            double dz = robot2_pos_.z - robot1_pos_.z;
+
+            double distance = std::sqrt(dx*dx + dy*dy + dz*dz);
+
+            RCLCPP_INFO(this->get_logger(), 
+                "Robot1 position: [%.3f, %.3f, %.3f]",
+                robot1_pos_.x, robot1_pos_.y, robot1_pos_.z);
+            
+            RCLCPP_INFO(this->get_logger(), 
+                "Robot2 position: [%.3f, %.3f, %.3f]",
+                robot2_pos_.x, robot2_pos_.y, robot2_pos_.z);
+            
+            RCLCPP_INFO(this->get_logger(), 
+                "Relative position (robot2 - robot1): [%.3f, %.3f, %.3f]",
+                dx, dy, dz);
+            
+            RCLCPP_INFO(this->get_logger(), 
+                "Distance between robots: %.3f meters\n",
+                distance);
+        }
+    }
+    
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr robot1_sub_;
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr robot2_sub_;
+    
+    geometry_msgs::msg::Point robot1_pos_;
+    geometry_msgs::msg::Point robot2_pos_;
+    bool robot1_received_ = false;
+    bool robot2_received_ = false;
+};
+
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<RelativePositionPrinter>());
+    rclcpp::shutdown();
+    return 0;
+}
